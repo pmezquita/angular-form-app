c99a5d8c75e8b393352b452d60973f10545ba854
pmezquita <ing.pedromezquita@gmail.com>
2023-06-29 13:38:13 -0600

    [ADD] Se inicia proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterOutlet} from "@angular/router";
+import {SharedModule} from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
