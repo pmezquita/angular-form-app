121a023dd9918573ff8fac891ae9de1f1c0e9d4b
pmezquita <ing.pedromezquita@gmail.com>
2023-06-30 13:08:46 -0600

    [ADD] Mensajes de errores formulario reactivo

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
-const rtx5090 = {
-  name: 'RTX5090',
-  price: 2500,
-  inStorage: 2,
-}
+// const rtx5090 = {
+//   name: 'RTX5090',
+//   price: 2500,
+//   inStorage: 2,
+// }
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -30,11 +30,35 @@ export class BasicPageComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.myForm.reset(rtx5090);
+    // this.myForm.reset(rtx5090);
+  }
+
+  isValidField = (field: string): boolean => this.myForm.controls[field].invalid && this.myForm.touched;
+
+  getFieldError = (field: string): string | null => {
+    if (!this.myForm.controls[field] && !this.myForm.controls[field].errors) return null;
+
+    const errors = this.myForm.controls[field].errors!;
+
+    for (const key of Object.keys(errors)) {
+      console.log(key);
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${errors[key].requiredLength} caracteres`;
+      }
+    }
+
+    return null;
   }
 
   onSave = (): void => {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.valid);
     this.myForm.reset({price: 0, inStorage: 0});
