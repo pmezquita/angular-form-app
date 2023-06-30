5052eb6b7b2ed869c5ff6bfc06f5d0fdef3c1fa6
pmezquita <ing.pedromezquita@gmail.com>
2023-06-30 14:18:35 -0600

    [ADD] Se inicia Formulario Dinamico

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
 })
 export class DynamicPageComponent {
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', [Validators.required]],
+      ['Death Stranding', [Validators.required]],
+    ]),
+  });
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  onSubmit = (): void => {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 
 }
