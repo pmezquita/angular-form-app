2165a4b5fe50d10ad1b50a3f4771b4374b95ff04
pmezquita <ing.pedromezquita@gmail.com>
2023-06-30 11:36:03 -0600

    [ADD] Se inicia formulario reactivo

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class BasicPageComponent {
 
+  // myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  myForm: FormGroup = this.fb.group({
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
+
+
+  constructor(private fb: FormBuilder) {
+  }
+
+
+  onSave = (): void => {
+    console.log(this.myForm.value);
+  }
 }
