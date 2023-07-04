fbab240fd515856409cf39a278683bb95807201a
pmezquita <ing.pedromezquita@gmail.com>
2023-07-04 14:26:48 -0600

    [ADD] Validacion email y patrones Formulario Register

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {cantBeStrider} from "../../../shared/validators/validators";
+import * as customValidators from "../../../shared/validators/validators";
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -11,14 +11,14 @@ export class RegisterPageComponent {
   }
 
   myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required, cantBeStrider]],
+    name: ['', [Validators.required, Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(customValidators.emailPattern)]],
+    username: ['', [Validators.required, customValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   });
 
-  isValidField = (field: string) =>{
+  isValidField = (field: string) => {
     //TODO: obtener validación de un servicio
   }
 
