6cab6893d4538437ac85a0de306d5de948ad87dd
pmezquita <ing.pedromezquita@gmail.com>
2023-07-04 14:13:50 -0600

    [ADD] Validator Functions Formulario Register

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {cantBeStrider} from "../../../shared/validators/validators";
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -12,7 +13,7 @@ export class RegisterPageComponent {
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required]],
     email: ['', [Validators.required]],
-    username: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   });
