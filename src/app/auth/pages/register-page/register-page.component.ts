94e7bfc2b8467416675d5bc090325ef41d671a96
pmezquita <ing.pedromezquita@gmail.com>
2023-07-04 15:09:07 -0600

    [ADD] Validadores asíncronos Formulario Register

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ValidatorsService} from "../../../shared/services/validators.service";
+import {EmailValidator} from "../../../shared/validators/email-validator.service";
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -10,12 +11,14 @@ export class RegisterPageComponent {
   constructor(
     private fb: FormBuilder,
     private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator,
   ) {
   }
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.pattern(ValidatorsService.firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(ValidatorsService.emailPattern)]],
+    // email: ['', [Validators.required, Validators.pattern(ValidatorsService.emailPattern)], [new EmailValidator()]],
+    email: ['', [Validators.required, Validators.pattern(ValidatorsService.emailPattern)], [this.emailValidator]],
     username: ['', [Validators.required, this.validatorsService.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
