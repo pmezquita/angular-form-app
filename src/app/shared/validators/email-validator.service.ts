84b5aa26bf43483e648c4ef1ffb91ccfdf2c37ec
pmezquita <ing.pedromezquita@gmail.com>
2023-07-04 15:23:02 -0600

    [ADD] Validadores asíncronos con observable Formulario Register

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -6,17 +6,38 @@ import {delay, Observable, of} from "rxjs";
   providedIn: 'root'
 })
 export class EmailValidator implements AsyncValidator {
+
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log({email});
 
-    return of({
-      emailTaken: true,
-    })
-      .pipe(
-        delay(1500),
+    const httpCallObservable =
+      new Observable<ValidationErrors | null>(subscriber => {
+        if (email == 'ing.pedromezquita@gmail.com') {
+          subscriber.next({emailTaken: true});
+          subscriber.complete();
+          return;
+        }
+
+        subscriber.next(null);
+        subscriber.complete();
+      }).pipe(
+        delay(3000),
       );
+
+    return httpCallObservable;
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //   const email = control.value;
+  //   console.log({email});
+  //
+  //   return of({
+  //     emailTaken: true,
+  //   })
+  //     .pipe(
+  //       delay(1500),
+  //     );
+  // }
 
 
 }
